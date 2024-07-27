7f07e02cdc033fa50bebfcdbd86a0659ffbe3c5e
yunus emre <yemrekara41@gmail.com>
2024-07-27 14:15:43 +0300

    mediatorV2

--- Core/CarBook.Application/Features/Mediator/Handlers/FooterAddressHandlers/RemoveFooterAddressCommandHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/FooterAddressHandlers/RemoveFooterAddressCommandHandler.cs
@@ -13,6 +13,12 @@ namespace CarBook.Application.Features.Mediator.Handlers.FooterAddressHandlers
     public class RemoveFooterAddressCommandHandler : IRequestHandler<RemoveFooterAddressCommand>
     {
         private readonly IRepository<FooterAddress> _repository;
+
+        public RemoveFooterAddressCommandHandler(IRepository<FooterAddress> repository)
+        {
+            _repository = repository;
+        }
+
         public async Task Handle(RemoveFooterAddressCommand request, CancellationToken cancellationToken)
         {
             var values = await _repository.GetByIdAsync(request.Id);
